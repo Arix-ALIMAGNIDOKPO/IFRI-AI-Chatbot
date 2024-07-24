0b5f961e158c0de18a5329e350e4a84b0b8ab3f0
Arix ALIMAGNIDOKPO <127417709+Arix-ALIMAGNIDOKPO@users.noreply.github.com>
2024-07-24 14:10:45 +0100

    Update V1 Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,8 +1,5 @@
 __import__('pysqlite3')
-''''__import__('pysqlite3')
 import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')''''
 import streamlit as st
 
 #Importations
